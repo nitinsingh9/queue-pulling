53277958bd383f1e09a0d3f15eb711a0ec0ed1a1
Nitin Singh <nitinpsingh46@gmail.com>
2024-10-24 12:08:43 +0530

    simple redis+woker setups

---
+++ server/src/index.ts
@@ -0,0 +1,45 @@
+const express = require("express")
+import { Request, Response } from "express";
+
+import { createClient } from "redis";
+
+
+const app = express();
+app.use(express.json());
+
+const client = createClient();
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+app.post("/submit", async (req: Request, res: Response) => {
+    const problemId = req.body.problemId;
+    const code = req.body.code;
+    const language = req.body.language;
+
+    try {
+        let i=0;
+        while(i<10){
+            await client.lPush("problems", JSON.stringify({ code, language, problemId }));
+            i++;
+        }
+        // Store in the database
+        res.status(200).send("Submission received and stored.");
+    } catch (error) {
+        console.error("Redis error:", error);
+        res.status(500).send("Failed to store submission.");
+    }
+});
+
+async function startServer() {
+    try {
+        await client.connect();
+        console.log("Connected to Redis");
+
+        app.listen(3000, () => {
+            console.log("Server is running on port 3000");
+        });
+    } catch (error) {
+        console.error("Failed to connect to Redis", error);
+    }
+}
+
+startServer();
